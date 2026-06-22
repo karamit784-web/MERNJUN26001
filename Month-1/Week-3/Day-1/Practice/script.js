cc8b005ac37b1f2fa8e3a742f8ba547aeb0a6325
karamit784-web <karamit784@gmail.com>
2026-06-22 12:57:16 +0530

    Javascript in Internal way and External way

---
+++ Month-1/Week-3/Day-1/Practice/script.js
@@ -0,0 +1,70 @@
+console.log("Hello World");
+
+a=30;
+console.log(a);
+
+A=40;
+console.log(A);
+
+st1="Amit";
+console.log(st1);
+
+_=80;
+console.log(_);
+
+$=70;
+console.log($);
+
+full_name="Hii";
+console.log(full_name);
+
+//var let const
+
+// scope
+    //global
+    //function
+    //block
+
+//declare at global level
+var a=40;
+console.log(a);
+
+//delare at block level
+{
+    var b=50;
+    console.log("Block Level Access",b);
+}
+
+console.log("Outside The Block",b);
+
+//declare at function level
+function getV(){
+    var d=55;
+    console.log(d)
+}
+getV()
+
+//The way of declaration
+
+console.log("Amit")
+
+//var num;
+var num=90;
+//console.log(num);
+
+num=40;
+console.log(num);
+
+var num1=70;
+console.log(num1);
+
+var num=100;
+console.log(num);
+
+//Let
+
+let u;
+console.log(u);
+
+u=80;
+console.log(u);
